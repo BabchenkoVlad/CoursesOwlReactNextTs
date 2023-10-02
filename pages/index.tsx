6e7ed4949c7e7d49fbcde843970eb22d689fb299
BabchenkoVlad <babch.vl@gmail.com>
2023-10-02 16:05:31 +0300

    h1 + button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-
+import React from "react";
+import { Button, Htag } from "@/components/index";
 
 
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      sdfsfa
-    </div>
+    <>
+      <Htag tag='h1'>Текст</Htag>
+      <Button appearance="primary">кнопка</Button>
+      <Button appearance="ghost">кнопка 2</Button>
+    </>
   )
 }
