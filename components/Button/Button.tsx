aa2e77313ae8137f066d5a08d4eda7754f08943a
BabchenkoVlad <babch.vl@gmail.com>
2023-10-03 15:08:34 +0300

    button props

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,15 +3,16 @@ import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
   return (
     <button
-      className={cn(styles.button, {
-          [styles.primary]: appearance == 'primary',
-          [styles.ghost]: appearance == 'ghost'
+      className={cn(styles.button, className, {
+        [styles.primary]: appearance == 'primary',
+        [styles.ghost]: appearance == 'ghost'
       })}
+      {...props}
     >
-      
+
       {children}
     </button>
   )
